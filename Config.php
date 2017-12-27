b12950faba3fe2ff5ae0965e817f51a4fa9c61f5
waskito <yg.waskito@gmail.com>
2017-12-27 19:14:06 +0700

    Initial Commit

---
+++ Config.php
@@ -0,0 +1,10 @@
+<?php 
+	define('DB_USERNAME','root');
+	define('DB_PASSWORD','');
+	define('DB_NAME','fcm');
+	define('DB_HOST','localhost');
+
+	//defined a new constant for firebase api key
+	define('FIREBASE_API_KEY', 'AIzaSyADo-W0jL1mVmSBWDPLIl0L4wiWqFqOSrs');
+
+	
